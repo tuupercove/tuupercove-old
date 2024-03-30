369da969c461200bf2434d460a82f8f1f5b15754
nicekun.workstation <nicekun.workstation@gmail.com>
2024-03-30 13:54:32 +0000

    add screenfetch

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -17,6 +17,7 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "tabnine-vscode"
     ];
     workspace = {
       # Runs when a workspace is first created with this `dev.nix` file
