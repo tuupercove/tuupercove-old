091ee34248474b44d768a83859f4e329b1ed4b1e
nicekun.workstation <nicekun.workstation@gmail.com>
2024-04-05 20:26:54 +0000

    404 is welcome

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link'
+import Header from 'next/head'
 
 export default function ForZeroForErrPage() {
     return (
         <div>
+            <Header>
+                <title>404 is welcome - TuuperCove</title>
+            </Header>
             <p>Huh? 404 is found. ummm something is wrong bro.</p>
             <Link href='/'>Return to My home</Link>
         </div>
