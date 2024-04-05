27e2adde0c869d5d6bdbdad2d848e470c342f1cb
nicekun.workstation <nicekun.workstation@gmail.com>
2024-04-05 12:28:56 +0000

    fix 2 deploy in vercel

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link'
+
 export default function ForZeroForErrPage() {
     return (
         <div>
             <p>Huh? 404 is found. ummm something is wrong bro.</p>
+            <Link href={'/'}>Return to My home</Link>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <p>404 Bug is found!</p>
+      <p>Hmm it not finlished, please try again later</p>
+      <Link href={'/try'}>Try to click here!</Link>
     </main>
   );
 }
