e3e5a6dc614aab316d0c6463b2762b32dff7208b
nicekun.workstation <nicekun.workstation@gmail.com>
2024-04-05 18:44:22 +0000

    modified:   tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -48,25 +48,6 @@ const config: Config = {
         },
       }, // common layout tokens (applied to all themes)
       themes: {
-        extend: {
-          colors: {
-            white: "#FFFFFF",
-            black: "#000000",
-            blue: {
-              50: "#e6f1fe",
-              100: "#cce3fd",
-              200: "#99c7fb",
-              300: "#66aaf9",
-              400: "#338ef7",
-              500: "#006FEE",
-              600: "#005bc4",
-              700: "#004493",
-              800: "#002e62",
-              900: "#001731",
-            },
-            // .. rest of the colors
-          },
-        },
         light: {
           layout: {
             hoverOpacity: 0.8, //  this value is applied as opacity-[value] when the component is hovered
@@ -99,6 +80,25 @@ const config: Config = {
             },
           },
         },
+        // extend: {
+        //   colors: {
+        //     white: "#FFFFFF",
+        //     black: "#000000",
+        //     blue: {
+        //       50: "#e6f1fe",
+        //       100: "#cce3fd",
+        //       200: "#99c7fb",
+        //       300: "#66aaf9",
+        //       400: "#338ef7",
+        //       500: "#006FEE",
+        //       600: "#005bc4",
+        //       700: "#004493",
+        //       800: "#002e62",
+        //       900: "#001731",
+        //     },
+        //     // .. rest of the colors
+        //   },
+        // },
       },
     }),
   ]
