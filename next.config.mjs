a869d596f225bdf951abc53615e55ed4c3977c12
nicekun.workstation <nicekun.workstation@gmail.com>
2024-06-06 10:50:33 +0000

    fix cloudflare pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 import pkg from './next-i18next.config.js';
 import { loadCustomBuildParams } from './next-utils.config.js';
 
+//////
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
+
+// note: the if statement is present because you
+//       only need to use the function during development
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform()
+}
+//////
+
 const isProd = process.env.NODE_ENV === 'production'
 
 const {i18n} = pkg;
